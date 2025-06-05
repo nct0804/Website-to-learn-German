0c68fffdacd43e70ce14f5aebd84facea8e38337
Nguyen, Chi Thien <darealthien@gmail.com>
2025-06-05 21:11:34 +0200

    feat: add route's lesson in index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import lessonsRoutes from './modules/lessons/lessons.routes';
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Routes
+app.use('/api/lessons', lessonsRoutes);
+
+//Root
 app.get('/', (_, res) => {
   res.send('GermanGains backend is running ');
 });
