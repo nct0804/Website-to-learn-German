c909e349fae92bae1ae186dc8e97cb51e7fc31c6
Nguyen, Chi Thien <darealthien@gmail.com>
2025-06-06 23:28:21 +0200

    feat: add modules route to index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,14 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import lessonsRoutes from './modules/lessons/lessons.routes';
 import coursesRoutes from './modules/courses/courses.routes';
+import modulesRoutes from './modules/modules/modules.routes';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 //Routes
-app.use('/api/lessons', lessonsRoutes);
+app.use('/api/lesson', lessonsRoutes);
 app.use('/api/courses', coursesRoutes);
+app.use('/api/modules', modulesRoutes);
 
 //Root
 app.get('/', (_, res) => {
