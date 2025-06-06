0b77409f45fd1851cd24503c3f3a826ebcd83b47
Nguyen, Chi Thien <darealthien@gmail.com>
2025-06-06 18:07:21 +0200

    feat(api): add courses routes, controller

---
+++ backend/src/modules/courses/courses.routes.ts
@@ -0,0 +1,65 @@
+import express from 'express';
+import * as CourseController from './courses.controller';
+import { authenticate, checkRole } from '../../middleware/auth.middleware';
+import { handleError } from '../../utils/errors';
+
+const router = express.Router();
+
+// PubLic , without ADMIN
+router.get('/', async (req, res) => {
+  try {
+    await CourseController.getAllCourses(req, res);
+  } catch (error) {
+    handleError(error, res);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    await CourseController.getCourseById(req, res);
+  } catch (error) {
+    handleError(error, res);
+  }
+});
+
+// with ADMIN
+router.post(
+  '/', 
+  authenticate, 
+  checkRole(['admin']), 
+  async (req, res) => {
+    try {
+      await CourseController.createCourse(req, res);
+    } catch (error) {
+      handleError(error, res);
+    }
+  }
+);
+
+router.put(
+  '/:id', 
+  authenticate, 
+  checkRole(['admin']), 
+  async (req, res) => {
+    try {
+      await CourseController.updateCourse(req, res);
+    } catch (error) {
+      handleError(error, res);
+    }
+  }
+);
+
+router.delete(
+  '/:id', 
+  authenticate, 
+  checkRole(['admin']), 
+  async (req, res) => {
+    try {
+      await CourseController.deleteCourse(req, res);
+    } catch (error) {
+      handleError(error, res);
+    }
+  }
+);
+
+export default router;
