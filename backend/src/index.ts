55ab36a00dd3c0e8bbdfe33a32618bc1f3032945
Nguyen, Chi Thien <darealthien@gmail.com>
2025-06-09 20:52:22 +0200

    refactor: recorrect backend port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -66,7 +66,7 @@ app.use('/api/vocabulary', vocabularyRoutes);
 app.use('/api/exercises', exercisesRoutes);
 app.use('/api/exercise-options', exerciseOptionsRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running successfully on http://localhost:${PORT}`);
 });
