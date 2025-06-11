9943449fa19adad33cf8d813e98c22703deb2d16
Le <trgle0409@gmail.com>
2025-06-11 18:26:27 +0200

    feat: Learning page mit LearningContent/LearningTopBar erstmal hinzufügen

---
+++ frontend/src/components/learn/LearningContent.tsx
@@ -0,0 +1,3 @@
+export default function LearningContent() {
+  return <div>LearningContent</div>;
+}
--- frontend/src/pages/Learn.tsx
+++ frontend/src/pages/Learn.tsx
@@ -1,3 +1,13 @@
+import LearningTopBar from "../components/learn/LearningTopBar";
+import LearningContent from "../components/learn/LearningContent";
+
 export default function Learn() {
-  return null;
+  return (
+    <div className="flex flex-col h-screen">
+      <LearningTopBar />
+      <div className="flex flex-1 overflow-hidden h-full">
+        <LearningContent />
+      </div>
+    </div>
+  );
 }
