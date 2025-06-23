a23955b76d21b76b25e24fda2a9053f8ff120b4a
Nguyen, Chi Thien <darealthien@gmail.com>
2025-06-23 22:13:41 +0200

    feat: jtest configurations

---
+++ backend/jest.config.ts
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testMatch: ['**/tests/**/*.test.ts'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/index.ts',
+  ],
+  setupFilesAfterEnv: ['./tests/setup.ts'],
+};
