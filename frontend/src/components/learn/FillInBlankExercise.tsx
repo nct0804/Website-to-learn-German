3075ce97fcc7d95b1629d4aa59814b3d6bbdded1
Le <trgle0409@gmail.com>
2025-06-28 14:20:28 +0200

    feat: (UX) enter triggers the same as hitting check button

--- frontend/src/components/learn/FillInBlankExercise.tsx
+++ frontend/src/components/learn/FillInBlankExercise.tsx
@@ -6,6 +6,7 @@ export interface FillInBlankExerciseProps {
   suffix: string
   onSelect: (text: string) => void
   instruction?: string
+  onEnter?: () => void
 }
 
 export function FillInBlankExercise({
@@ -13,6 +14,7 @@ export function FillInBlankExercise({
   suffix,
   onSelect,
   instruction,
+  onEnter,
 }: FillInBlankExerciseProps) {
   const [inputValue, setInputValue] = useState("");
 
@@ -21,18 +23,25 @@ export function FillInBlankExercise({
     onSelect(e.target.value);
   }
 
+  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter" && onEnter) {
+      onEnter();
+    }
+  }
+
   return (
     <div className="flex flex-col space-y-8 w-full max-w-lg">
-      <h2 className="text-2xl font-semibold text-center mb-8 block">{instruction}</h2>
+      <h2 className="text-2xl font-bold text-center mb-8 block">{instruction + ""}</h2>
       <div className="flex justify-center items-center text-3xl space-x-2 mb-4">
-        <div className="text-3xl">{prefix}</div>
+        <div className="text-2xl">{prefix}</div>
         <input
           type="text"
           value={inputValue}
           onChange={handleChange}
+          onKeyDown={handleKeyDown}
           className="inline-block w-32 border-b-2 border-[#fbb124] text-center text-3xl bg-transparent focus:outline-none focus:border-[#408297]"
         />
-        <div className="text-3xl">{suffix}</div>
+        <div className="text-2xl">{suffix}</div>
       </div>
     </div>
   );
