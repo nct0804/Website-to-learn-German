1433118f80fe53279b85c841db676970f505e56d
Nguyen, Chi Thien <darealthien@gmail.com>
2025-07-07 22:00:14 +0200

    fix: delete hearts middleware

--- backend/src/middleware/hearts.middleware.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { PrismaClient } from '@prisma/client';
-import { BadRequestError } from '../utils/errors';
-
-// Todo : to check if a user has hearts before allowing them to attempt an exercise
-// Todo : write automation test
