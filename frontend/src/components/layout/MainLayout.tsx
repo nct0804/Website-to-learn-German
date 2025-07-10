e7e563c26c51f93828644a9ae729945000db00dc
Le <trgle0409@gmail.com>
2025-07-11 00:45:45 +0200

    responsive: ensure layout maintains its structure, prevent from collapsing on smaller screens

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function MainLayout() {
   return (
-    <div className="flex flex-col h-screen px-8 py-4 bg-[#FBFBFC]">
+    <div className="flex flex-col h-screen px-8 py-4 bg-[#FBFBFC] min-w-[1400px]">
       <TopBar />
       <div className="flex flex-1 overflow-hidden h-full">
         <LeftBar/>
