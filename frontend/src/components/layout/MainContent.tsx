77a9d6ccc4199971ab45d54d81a1e3a532bf0d39
Le <trgle0409@gmail.com>
2025-07-20 17:18:46 +0200

    responsive: add rules for xl, 2xl screen

--- frontend/src/components/layout/MainContent.tsx
+++ frontend/src/components/layout/MainContent.tsx
@@ -102,12 +102,16 @@ export default function MainContent() {
   const { data: lessonsByModule, loading: lessonsLoading, error: lessonsError } = useAllModulesLessonProgress(moduleIds);
 
   return (
-    <div className="flex-1 flex flex-col items-center overflow-auto max-w-3xl mx-auto bg-blue-50 rounded-3xl">
+    <div className="flex-1 flex flex-col items-center mx-auto bg-blue-50 rounded-3xl dark:bg-gray-800 max-w-3xl 2xl:max-w-4xl">
       {/* Hero Section: only show when no course is selected */}
       {!current && (
         <div className="w-full mb-8">
-          <div className="relative bg-gradient-to-r from-[#ffa600] via-[#ffa600] to-[#FFB124] rounded-3xl shadow-lg px-8 py-8 flex flex-col md:flex-row items-center md:items-center text-center md:text-left gap-8 overflow-hidden">
-            <div className="flex flex-col items-center justify-center md:items-start md:justify-start flex-1 z-10">
+          <div className="relative bg-gradient-to-r from-[#ffa600] via-[#ffa600] to-[#FFB124] 
+          rounded-3xl shadow-lg px-8 py-8 flex flex-col md:flex-row items-center 
+          md:items-center text-center md:text-left gap-8 overflow-hidden
+          dark:bg-gradient-to-b dark:from-[#05315B] dark:via-[#256996] dark:to-[#3B6978] dark:text-white">
+            <div className="flex flex-col items-center justify-center 
+            md:items-start md:justify-start flex-1 z-10">
               {/* <img src={Course1Icon} alt="Learning Path" className="scale-130 mb-4" /> */}
               <h1 className="text-3xl md:text-4xl font-extrabold text-white mb-2 drop-shadow">Your Learning Path</h1>
               <p className="text-lg text-white max-w-2xl mx-auto mb-2">Advance step by step through expertly designed German courses. Unlock new skills, earn XP, and celebrate your progress!</p>
@@ -135,7 +139,7 @@ export default function MainContent() {
           </div>
         </div>
       )}
-      <div className="w-full rounded-3xl px-8 py-8 shadow-lg dark:bg-gray-800 transition-colors duration-300">
+      <div className="w-full rounded-3xl shadow-lg dark:bg-gray-800 transition-colors duration-300 h-full ">
         {!current ? (
           <>
             
@@ -161,7 +165,7 @@ export default function MainContent() {
           current.modules.map((module) => {
             const lessons = lessonsByModule[module.id] || [];
             return (
-              <div key={module.id} className="">
+              <div key={module.id}>
                 {/* {module.isLocked ? (
                   // Show a hint for the next lesson (first lesson in the module)
                   lessons.length > 0 && lessons[0].title ? (
--- frontend/src/components/layout/RightBar.tsx
+++ frontend/src/components/layout/RightBar.tsx
@@ -3,7 +3,7 @@ import LeaderboardPanel from "../panels/LeaderboardPanel";
 
 export default function RightBar() {
     return (
-      <aside className="w-[30%] min-w-[250px] rounded-3xl transition-colors duration-300">
+      <aside className="w-[30%] w-[400px]  rounded-3xl transition-colors duration-300">
         <QuotePanel />
         <LeaderboardPanel />
       </aside>
